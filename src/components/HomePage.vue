d935ed65da03ca50c7531e1948bec6806cbbaa43
sypark <yspark2@yookgak.com>
2023-02-06 11:58:12 +0900

    feat: axios API 연결 테스트

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,6 +2,14 @@
   <div id="app">
     Home
 
+    <div v-if="loading">Page Loading ~</div>
+    <div v-else-if="err !== undefined">{{ err }}</div>
+    <div v-else>
+      api = {{ apiRes }}
+      <!-- <div v-for="board in boards" :key="board.id">
+        <router-link :to="'/b/' + board.id">{{ board.name }}</router-link>
+      </div> -->
+    </div>
     <ul>
       <li>
         <router-link to="/b/1">Board 1</router-link>
@@ -14,9 +22,61 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HomePage",
-  components: {},
+  data() {
+    return { loading: false, apiRes: "" };
+  },
+  created() {
+    this.fetchData();
+  },
+  watch: {
+    $route: {
+      handler: "fetchData()",
+      immediate: true,
+    },
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+
+      axios
+        .get("http://localhost:3000/Health", {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          param: {},
+        })
+        .then((res) => {
+          this.loading = false;
+          this.apiRes = res.data;
+        })
+        .catch((res) => {
+          this.loading = false;
+          this.err = res.response.data;
+        })
+        .finally(function () {});
+    },
+    fetchDataXml() {
+      this.loading = true;
+
+      let req = new XMLHttpRequest();
+
+      req.open("GET", "http://localhost:3000/Health", true);
+      req.send();
+
+      req.addEventListener("load", () => {
+        this.loading = false;
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.responseText),
+        };
+      });
+    },
+  },
 };
 </script>
 
